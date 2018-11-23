7c5eb68fe09728f45a674ece3756eeeb75edd928
zhangchen <zhangchen01@hc360.com>
2018-11-23 18:10:56 +0800

    p4p运行数据

--- dataweb/src/main/java/com/hc360/dataweb/controller/P4pOutcomeController.java
+++ dataweb/src/main/java/com/hc360/dataweb/controller/P4pOutcomeController.java
@@ -129,7 +129,7 @@ public class P4pOutcomeController {
      *
      * @param request
      * @param response
-     * @param flag  expend==>累计消耗信息  charge =>累计充值信息
+     * @param flag  expend==>累计消耗信息  charge =>累计充值信息 balance=>余额情况
      * @throws Exception
      */
     @RequestMapping(value = "/column3d", method = RequestMethod.GET, produces = {"application/xml", "application/json"})
@@ -145,9 +145,14 @@ public class P4pOutcomeController {
                 dataTypeList.add(DataType.P4PXIANJINEXPENDTOTAL.getType()); //现金消耗
             }
             if("charge".equals(flag)){
-                dataTypeList.add(DataType.P4PXUNICHARGETOTAL.getType());// 虚拟余额
-                dataTypeList.add(DataType.P4PFANDIANJINCHARGETOTAL.getType());// 返点金余额
-                dataTypeList.add(DataType.P4PXIANJINCHARGETOTAL.getType());// 现金余额
+                dataTypeList.add(DataType.P4PXUNICHARGETOTAL.getType());// 虚拟充值
+                dataTypeList.add(DataType.P4PFANDIANJINCHARGETOTAL.getType());// 返点金充值
+                dataTypeList.add(DataType.P4PXIANJINCHARGETOTAL.getType());// 现金充值
+            }
+            if("balance".equals(flag)){
+                dataTypeList.add(DataType.P4PXUNIBALANCE.getType());// 虚拟余额
+                dataTypeList.add(DataType.P4PFANDIANJINBALANCE.getType());// 返点金余额
+                dataTypeList.add(DataType.P4PXIANJINBALANCE.getType());// 现金余额
             }
             _dataMap  = p4pServiceImpl.columd3D(dataTypeList,6);
             _dataMap.put("errno",0);
